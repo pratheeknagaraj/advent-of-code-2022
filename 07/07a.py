69335fd37e363b6cd9a6b36b0ce34bb50caea7a7
Pratheek Nagaraj <2501134+pratheeknagaraj@users.noreply.github.com>
2023-03-06 22:00:34 +0000

    Add Day 07 Solutions

---
+++ 07/07a.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+
+with open('07_input', 'r') as f:
+    lines = f.readlines()
+
+class Directory:
+
+    def __init__(self, name, parent=None):
+        self.name = name
+        self.dirs = {}
+        self.files = {}
+        self.size = None
+
+        self.parent = parent
+
+    def __repr__(self):
+        return self.__str__
+
+    def __str__(self):
+        data = {
+            'name': self.name,
+            'dirs': self.dirs.keys(),
+            'files': self.files.items(),
+            'size': self.size,
+            'parent': self.parent.name if self.parent else 'None'
+        }
+
+        return str(data)
+
+root = Directory('/')
+
+cwd = root
+
+listing = False
+
+for line in lines:
+    l = line.strip()
+    if l.startswith('$'):
+        # Command
+        parts = l.split()
+        cmd = parts[1]
+        if cmd == 'cd':
+            target = parts[2]
+            listing == False
+            if target == '/':
+                cwd = root
+            elif target == '..':
+                cwd = cwd.parent
+            else:
+                cwd = cwd.dirs[target]
+        elif cmd == 'ls':
+            listing = True
+    else:
+        # Output
+        if listing == True:
+            val, name = l.split()
+            if val == 'dir':
+                if name not in cwd.dirs:
+                    cwd.dirs[name] = Directory(name, cwd)
+            else:
+                if name not in cwd.files:
+                    cwd.files[name] = int(val)
+
+
+def compute_size(node):
+    dir_size = 0
+    if node.dirs.keys():
+        for d in node.dirs.values():
+            dir_size += compute_size(d)
+
+    dir_size += sum(node.files.values())
+    node.size = dir_size
+
+    return dir_size
+
+compute_size(root)
+
+queue = [root]
+
+cap = 100000
+
+sum_total = 0
+
+while queue:
+    cur = queue.pop(0)
+    queue.extend(cur.dirs.values())
+
+    if cur.size <= cap:
+        sum_total += cur.size
+
+print(sum_total)
